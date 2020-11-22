5db2d79da0b764a13dce1204a8d8166ae53e2bac
VincentH-APCS <vhuang10@stuy.edu>
2020-11-21 23:23:55 -0500

    Simultaneously testing multiplying RationalNumbers and reducing them

--- Tester.java
+++ Tester.java
@@ -1,15 +1,15 @@
 public class Tester{
   public static void main(String[] args){
     RationalNumber a = new RationalNumber(2,5);
-    //RationalNumber b = new RationalNumber(3,0);
+    RationalNumber b = new RationalNumber(3,0);
     RationalNumber c = new RationalNumber(4,10);
-    RationalNumber d = new RationalNumber(12,14);
+    RationalNumber d = new RationalNumber(1,2);
     RationalNumber e = new RationalNumber(5,9);
-    RationalNumber f = new RationalNumber(-2,7);
+    RationalNumber f = new RationalNumber(-2,-4);
 
-    System.out.println(c.toString());
-    System.out.println(a.toString());
-    System.out.println(d.toString());
-    System.out.println(e.toString());
+    System.out.println(a.multiply(b));
+    System.out.println(a.multiply(c));
+    System.out.println(d.multiply(f));
+    System.out.println(f.multiply(d));
   }
 }
