b8bf127a6e65315a5deb1e1f71e7cc8a0bfe1d01
VincentH-APCS <vhuang10@stuy.edu>
2020-11-21 23:03:55 -0500

    the test for gcd

--- Tester.java
+++ Tester.java
@@ -4,14 +4,12 @@ public class Tester{
     RationalNumber b = new RationalNumber(3,0);
     RationalNumber c = new RationalNumber(4,10);
     RationalNumber d = new RationalNumber(12,14);
-    RationalNumber e = new RationalNumber(5,7);
-    RationalNumber f = new RationalNumber(-2,-5);
+    RationalNumber e = new RationalNumber(5,9);
+    RationalNumber f = new RationalNumber(-2,7);
 
-    System.out.println(a.toString());
-    System.out.println(b.toString());
-    System.out.println(c.toString());
-    System.out.println(d.toString());
-    System.out.println(e.toString());
-    System.out.println(f.toString());
+    System.out.println(RationalNumber.gcd(210,45));
+    System.out.println(RationalNumber.gcd(45,210));
+    System.out.println(RationalNumber.gcd(7,2));
+    System.out.println(RationalNumber.gcd(10,30));
   }
 }
