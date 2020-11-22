434720200691db5f6489c910946c84b6b211f9b8
VincentH-APCS <vhuang10@stuy.edu>
2020-11-21 22:19:39 -0500

    extensively tested multiple scenarios for the equals method

--- Tester.java
+++ Tester.java
@@ -1,16 +1,19 @@
 public class Tester{
   public static void main(String[] args){
     RationalNumber a = new RationalNumber(2,5);
-    RationalNumber b = new RationalNumber(1,9);
-    RationalNumber c = new RationalNumber(3,-4);
+    RationalNumber b = new RationalNumber(3,0);
+    RationalNumber c = new RationalNumber(4,10);
     RationalNumber d = new RationalNumber(4,0);
-    RationalNumber e = new RationalNumber(-5,-15);
+    RationalNumber e = new RationalNumber(2,5);
+    RationalNumber f = new RationalNumber(-2,-5);
 
-    System.out.println(a.reciprocal());
-    System.out.println(b.reciprocal());
-    System.out.println(c.reciprocal());
-    System.out.println(d.reciprocal());
-    System.out.println(e.reciprocal());
-  
+    System.out.println(a.equals(e));
+    System.out.println(a.equals(f));
+    System.out.println(e.equals(f));
+    System.out.println(f.equals(e));
+    System.out.println(b.equals(d));
+    System.out.println(a.equals(d));
+    System.out.println(e.equals(c));
+    System.out.println(b.equals(e));
   }
 }
