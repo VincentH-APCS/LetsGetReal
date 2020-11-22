7b9bf04bd43baeb946ab89913541144ace1b4b9c
VincentH-APCS <vhuang10@stuy.edu>
2020-11-21 22:04:28 -0500

    Tests to see if the Numerator and denominator are the problem of getValue

--- Tester.java
+++ Tester.java
@@ -1,14 +1,22 @@
 public class Tester{
   public static void main(String[] args){
-    RealNumber a = new RealNumber(4.0);
-    RealNumber b = new RealNumber(4.0);
-    RealNumber c = new RealNumber(7.0);
-    RealNumber d = new RealNumber(4.001);
-    RealNumber e = new RealNumber(5.0);
+    RationalNumber a = new RationalNumber(2,5);
+    RationalNumber b = new RationalNumber(1,9);
+    RationalNumber c = new RationalNumber(3,-4);
+    RationalNumber d = new RationalNumber(4,0);
+    RationalNumber e = new RationalNumber(-5,-15);
 
-    System.out.println(a.getValue());
-    System.out.println(a.subtract(b));
-    System.out.println(c.subtract(e));
-    System.out.println(e.subtract(c));
+    System.out.println(a.getNumerator());
+    System.out.println(a.getDenominator());
+    System.out.println(c.getNumerator());
+    System.out.println(c.getDenominator());
+    System.out.println(d.getNumerator());
+    System.out.println(d.getDenominator());
+    
+    /*System.out.println(a.getValue());
+    System.out.println(b.getValue());
+    System.out.println(c.getValue());
+    System.out.println(d.getValue());
+    System.out.println(e.getValue());*/
   }
 }
