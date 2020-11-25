6741733570107ef0561fde1e011b6d292a928c3c
VincentH-APCS <vhuang10@stuy.edu>
2020-11-25 01:53:14 -0500

    We seem to be good!

--- Number.java
+++ Number.java
@@ -1,4 +1,5 @@
 public abstract class Number{
+
   public abstract double getValue();
 
   public int compareTo(Number other){
@@ -20,11 +21,12 @@ public boolean equals(Number other){
     if(this.getValue() != 0 && other.getValue() == 0){
       return false;
     }
-    if( Math.abs(this.getValue() - other.getValue()) / this.getValue() < 0.00001 ||
-        Math.abs(other.getValue() - this.getValue()) / this.getValue() < 0.00001){
+    if( Math.abs(this.getValue() - other.getValue()) / Math.abs(this.getValue()) < 0.00001 ||
+        Math.abs(other.getValue() - this.getValue()) / Math.abs(this.getValue()) < 0.00001){
       return true;
     }
     else return false;
   }
 
 }
+/ :]
